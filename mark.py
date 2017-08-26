9eb00bb4bab07bc454635b94edff76552226ca84
Tyler Hoyt <tylerhoyt93@gmail.com>
2017-08-25 20:24:38 -0400

    Cleaned up mark.py

--- mark.py
+++ mark.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from PIL import Image
 from hilbert import Hilbert
+import sys
 import math
 import binascii
 
@@ -14,7 +15,7 @@ import binascii
 # 11 --> X  , Y-1
 
 # returns the new color of the pixel with the encoded data
-def _calculatePixelCode(x, y, bKey, hilbert):
+def calculatePixelCode(x, y, bKey, hilbert):
     code = [0,0,0]
 
     # Get the distance along the curve a coordinate is in 2D space.
@@ -48,7 +49,7 @@ def _calculatePixelCode(x, y, bKey, hilbert):
     return code
 
 # returns the new color value
-def _changeColors(code, pixel):
+def changeColors(code, pixel):
     for i in range(3):
         if((pixel[i] % 2) != code[i]):
             if(pixel[i] > 265/2):
@@ -57,43 +58,48 @@ def _changeColors(code, pixel):
                 pixel[i] += 1
     print(pixel)
 
-inputImage = "image.png"
-userKey = "S-THIS IS THE KEY HAHAHAHAHAHA-E"
-maxKeyLength = 32
+
+def main():
+    inputImage = sys.argv[1]
+    userKey = "S-THIS IS THE KEY HAHAHAHAHAHA-E"
+    maxKeyLength = 32
 
 
-# Convert key to binary
-if(len(userKey) > maxKeyLength):
-    quit()
-binKey = bin(int(binascii.hexlify(userKey), 16))[2:]
-binKey = binKey.zfill(maxKeyLength * 8)
+    # Convert key to binary
+    if(len(userKey) > maxKeyLength):
+        quit()
+    binKey = bin(int(binascii.hexlify(userKey), 16))[2:]
+    binKey = binKey.zfill(maxKeyLength * 8)
 
 
-# load the image
-img = Image.open(inputImage)
-# create the pixel map
-pixels = img.load()
+    # load the image
+    img = Image.open(inputImage)
+    # create the pixel map
+    pixels = img.load()
 
-# Compute the number of hilbert curve itterations to construct a cube encompusing
-# the entire immage.
-hilbertItterations = int(math.ceil(math.sqrt(max(img.size[0], img.size[1]))))
-hilbert = Hilbert(hilbertItterations, 2)
+    # Compute the number of hilbert curve itterations to construct a cube encompusing
+    # the entire immage.
+    hilbertItterations = int(math.ceil(math.sqrt(max(img.size[0], img.size[1]))))
+    hilbert = Hilbert(hilbertItterations, 2)
 
-for j in range(img.size[0]):        # for every col:
-    for i in range(img.size[1]):    # For every row
-        print(pixels[i,j])
-        lstPx = list(pixels[i,j])     # Convert tuple of color to list
-        code = _calculatePixelCode(i, j, binKey, hilbert)
-        print("Pre= " + str(pixels[i,j]))
-        _changeColors(code, lstPx)
-        pixels[i,j] = tuple(lstPx)
-        print("Post= " + str(pixels[i,j]))
-        print
-print(binKey)
-print(len(binKey))
-n = int('0b' + binKey, 2)
-key = binascii.unhexlify('%x' % n)
-print(key)
+    for j in range(img.size[0]):        # for every col:
+        for i in range(img.size[1]):    # For every row
+            print(pixels[i,j])
+            lstPx = list(pixels[i,j])     # Convert tuple of color to list
+            code = calculatePixelCode(i, j, binKey, hilbert)
+            print("Pre= " + str(pixels[i,j]))
+            changeColors(code, lstPx)
+            pixels[i,j] = tuple(lstPx)
+            print("Post= " + str(pixels[i,j]))
+            print
+    print(binKey)
+    print(len(binKey))
+    n = int('0b' + binKey, 2)
+    key = binascii.unhexlify('%x' % n)
+    print(key)
 
 
-img.save("out.png")
+    img.save("out.png")
+
+if __name__ == "__main__":
+    main()
